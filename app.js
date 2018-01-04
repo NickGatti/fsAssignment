327439e8aa330f4308595566376cea5dd860ffa4
NickGatti <nick.gatti@gmail.com>
2018-01-04 13:37:43 -0700

    challenge4

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ let names = fs.readFileSync( './challenge2/info.txt', 'utf-8' )
 names = names + 'Nick\nNick\nCarlos\nJennifer\n'
 fs.writeFileSync( './challenge2/info.txt', names )
 
-fs.rename( './challenge3/binfo.txt', './challenge3/info.txt' )
+//fs.rename( './challenge3/binfo.txt', './challenge3/info.txt' )
+
+let dir = './challenge4/copyfolder'
+
+if ( !fs.existsSync( dir ) ) {
+    fs.mkdirSync( dir );
+}
+fs.createReadStream( './challenge4/info.txt' ).pipe( fs.createWriteStream( './challenge4/copyfolder/info.txt' ) );
