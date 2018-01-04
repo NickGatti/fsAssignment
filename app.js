84478c8bfee4c0e206a8aa1c437a72b916897c24
NickGatti <nick.gatti@gmail.com>
2018-01-04 13:34:31 -0700

    challenge3

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ console.log( fs.readFileSync( './challenge1/info.txt', 'utf-8' ) );
 let names = fs.readFileSync( './challenge2/info.txt', 'utf-8' )
 names = names + 'Nick\nNick\nCarlos\nJennifer\n'
 fs.writeFileSync( './challenge2/info.txt', names )
+
+fs.rename( './challenge3/binfo.txt', './challenge3/info.txt' )
