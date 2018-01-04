62a73f46cc9e0733206524b5e22059f6057913c1
NickGatti <nick.gatti@gmail.com>
2018-01-04 11:54:05 -0700

    challenge2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var fs = require( 'fs' );
 
 console.log( fs.readFileSync( './challenge1/info.txt', 'utf-8' ) );
+
+let names = fs.readFileSync( './challenge2/info.txt', 'utf-8' )
+names = names + 'Nick\nNick\nCarlos\nJennifer\n'
+fs.writeFileSync( './challenge2/info.txt', names )
